61784cc7d1b9f4905ac5ece6082b8636934339e7
Logan King <59426296+EagleAim@users.noreply.github.com>
2023-11-14 23:49:41 -0800

    complexity is 3 everything is cool blocking everything i put

--- script.js
+++ script.js
@@ -11,12 +11,16 @@ console.log(min(0, -10));
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
+    
+    
+    const calculateCircumference = (n) => {
+        let diameter = 10;
+        let pi = 3.14;
+        return diameter => pi * n
+    }
 
-let diameter = 10
-if (diameter > 0) {
-    let pi = 3.14
-}
-
+    console.log(calculateCircumference(5));
+    
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
